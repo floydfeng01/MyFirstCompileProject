42174a13d246162303a5377740ad7e3a35385cfa
fengchangwen <fengchangwen@f-road.com.cn>
2018-07-24 23:47:19 +0800

    添加项目代码

---
+++ mylib/src/main/java/com/froad/mylib/MyTest.java
@@ -0,0 +1,12 @@
+package com.froad.mylib;
+
+/**
+ * Created by FW on 2018/7/24.
+ */
+
+public class MyTest {
+
+    public double addition (double d1, double d2) {
+        return d1 + d2;
+    }
+}
