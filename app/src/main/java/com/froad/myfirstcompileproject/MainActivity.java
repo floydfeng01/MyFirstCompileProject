efc1e85e23a74797690a825deb8a7192d0c69723
fengchangwen <fengchangwen@f-road.com.cn>
2018-09-03 17:54:45 +0800

    修改版本配置信息

--- app/src/main/java/com/froad/myfirstcompileproject/MainActivity.java
+++ app/src/main/java/com/froad/myfirstcompileproject/MainActivity.java
@@ -1,13 +1,22 @@
 package com.froad.myfirstcompileproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.froad.mylib.MyTest;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyTest myTest = new MyTest();
+        double d = myTest.addition(1,2);
+        Log.d(TAG, "onCreate>>>d:" + d);
     }
 }
